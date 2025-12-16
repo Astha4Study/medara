b6380827de682d3e641921f2826a310f73f1ed6c
Astha <iyandabes1@gmail.com>
2025-12-16 15:34:17 +0700

    add: table pemeriksaan fisik

---
+++ database/migrations/2025_12_16_063139_create_pemeriksaan_fisik_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pemeriksaan_fisik', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pasien_id')->constrained('pasien')->cascadeOnDelete();
+            $table->foreignId('klinik_id')->constrained('klinik')->cascadeOnDelete();
+            $table->decimal('berat_badan', 5, 2)->nullable();
+            $table->decimal('tinggi_badan', 5, 2)->nullable();
+            $table->integer('suhu_tubuh')->nullable();
+            $table->string('tekanan_darah', 10)->nullable();
+            $table->text('kondisi_khusus')->nullable();
+            $table->foreignId('created_by')->constrained('users')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pemeriksaan_fisik');
+    }
+};
